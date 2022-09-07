5940ce181e09ba219d50bc46b6805c5bbdf738b8
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-09-07 21:41:06 +0530

    updated

---
+++ todoapp/src/components/App/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import React from 'react';
+import List from '../Util/List';
+
+function App() {
+  return (
+    
+    <>
+
+
+<List/>
+    </>
+  );
+}
+
+export default App;
