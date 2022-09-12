62a07944675f56e00c5e5c5841e57cb5d2ee30eb
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-09-12 22:07:09 +0530

    ecommerce website

---
+++ ecommerce/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Layouts/Header';
+import Footer from './components/Layouts/Footer';
+import Error from './components/Layouts/Error';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Contact from './components/Pages/Contact';
+
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    
+    <Header />
+
+    
+    <Router>
+      <Link to ="/">Home</Link>
+      <Link to ="/about">About</Link>
+      <Link to ="/contact">Contact</Link>
+
+      <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path="/contact">
+            <Contact/>
+          </Route>
+          <Route exact path="*">
+            <Error />
+          </Route>
+          
+      </Switch>
+     
+    </Router>
+
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/components/Layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Footer Part
+    </div>
+  )
+}
+
+export default Footer
