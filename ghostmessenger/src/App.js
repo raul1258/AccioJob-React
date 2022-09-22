dafebf6674b32164d5e48b1238165b6c97637ecc
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-09-22 22:57:24 +0530

    firebase

---
+++ ghostmessenger/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SaveResponse from './component/SaveResponse';
+
+function App() {
+  return (
+    <>
+    <SaveResponse/>
+    </>
+  );
+}
+
+export default App;
---
+++ ghostmessenger/src/component/SaveResponse.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function SaveResponse() {
+
+    const [userName, setUserName]= React.useState('');
+    const [userMessage, setUserMessage] = React.useState('');
+
+    const handleUserNameChange = (event) => {
+        setUserName(event.target.value);
+    }
+    
+    const handleUserMessageChange = (event) => {
+        setUserMessage(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();  //prevent our form to reload it
+        console.log('userName : ', userName)
+        console.log('userMessage: ', userMessage);
+
+        // create  (crud)
+
+        fetch('https://accio-learning-default-rtdb.asia-southeast1.firebasedatabase.app/messages.json',
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                    userName: userName,
+                    userMessage: userMessage
+                })
+        }
+        )
+        .then(res => res.json())   
+        .then(data => console.log(data))
+
+    }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter Name" onChange={handleUserNameChange}/>
+        <input type="text" placeholder="Enter Your Message" onChange={handleUserMessageChange}/>
+        <button type='submit'>Submit</button>
+        
+      </form>
+    </div>
+  )
+}
+
+export default SaveResponse
